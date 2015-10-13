4116a5c86ca8cdb70fd18c01c367ddb7023a8932
fanyou <youfan.noey@gmail.com>
2015-10-13 19:35:18 -0400

    more heavy test case passed

--- tests/skip_list_test.cc
+++ tests/skip_list_test.cc
@@ -2,23 +2,23 @@
 #include <iostream>
 
 void test01() {
-  noey::skip_list<int> list(0, 100);
-  for (int i = 0; i < 100; ++i) {
+  noey::skip_list<int> list(0, 10000);
+  for (int i = 1; i < 10000; ++i) {
     list.insert(i);
   }
   assert(!list.empty());
-  for (int i = 0; i < 100; ++i) {
+  for (int i = 1; i < 10000; ++i) {
     list.erase(i);
   }
   assert(list.empty());
 }
 
 void test02() {
-  noey::skip_list<int> list(0, 100);
-  for (int i = 0; i < 100; ++i) {
+  noey::skip_list<int> list(0, 10000);
+  for (int i = 1; i < 10000; ++i) {
     list.insert(i);
   }
-  for (int i = 0; i < 100; ++i) {
+  for (int i = 1; i < 10000; ++i) {
     list.find(i);
   }
 }
